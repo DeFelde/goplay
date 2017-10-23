dd78f34f43914dc5b809ac5e2cbf4be94349877a
Blue <cosinori@gmail.com>
2017-10-23 19:47:21 +0200

    schleife erweitert

--- Uhr/Clock.go
+++ Uhr/Clock.go
@@ -6,21 +6,21 @@ import (
 )
 
 func main() {
-	hour := 8
+	//hour := 8
 	minute := 30
-	second := 0
+	//	second := 0
 
-	for hour < 10 {
+	for hour := 8; hour < 10; hour++ {
 		for minute < 60 {
-			for second < 60 {
+			for second := 0; second < 60; second++ {
 				fmt.Println(hour, minute, second)
 				time.Sleep(time.Second)
-				second = second + 1
+				//				second = second + 1
 			}
 			minute++
-			second = 0
+			//			second = 0
 		}
-		hour++
+		//		hour++
 		minute = 0
 	}
 }
