aedf12b5cd4cc3754180a1f7d488188a664f18c3
Blue <cosinori@gmail.com>
2017-10-27 23:00:35 +0200

    Grundlagen & Arrays

--- quadrat-fkt/quadratf.go
+++ quadrat-fkt/quadratf.go
@@ -15,32 +15,42 @@ import (
 func main() {
 	//	fmt.Println(3, "3")
 	//	fmt.Println(os.Args)
+	fmt.Printf("os.Args=%#v\n", os.Args)
 	if len(os.Args) != 4 {
 		fmt.Println("Genau drei Eingabewerte vonnöten!")
 		return
 	}
-	a, err := strconv.ParseFloat(os.Args[1], 64)
-	if err != nil {
-		fmt.Println("ungültiger Wert für a")
-		return
+	var params [3]float64
+	for i := 0; i < 3; i++ {
+		x, err := strconv.ParseFloat(os.Args[i+1], 64)
+		//fmt.Printf("a=%f\n", x)
+		if err != nil {
+			fmt.Printf("ungültiger Wert für a(%s): %s\n", os.Args[1], err)
+			return
+		}
+		params[i] = x
 	}
-	b, err := strconv.ParseFloat(os.Args[2], 64)
-	if err != nil {
-		fmt.Println("ungültiger Wert für b")
-		return
-	}
-	c, err := strconv.ParseFloat(os.Args[3], 64)
-	if err != nil {
-		fmt.Println("ungültiger Wert für c")
-		return
-	}
-	//	var (
-	//		a float64 = 3
-	//		b float64 = 4
-	//		c float64 = 17
-	//	)
-	p := b / a
-	q := c / a
+	fmt.Printf("%.1f x^2 + %.1f x + %.1f = 0\n", params[0], params[1], params[2])
+
+	//	a, err := strconv.ParseFloat(os.Args[1], 64)
+	//	fmt.Printf("a=%f\n", a)
+	//	if err != nil {
+	//		fmt.Printf("ungültiger Wert für a(%s): %s\n", os.Args[1], err)
+	//		return
+	//	}
+	//	b, err := strconv.ParseFloat(os.Args[2], 64)
+	//	if err != nil {
+	//		fmt.Println("ungültiger Wert für b")
+	//		return
+	//	}
+	//	c, err := strconv.ParseFloat(os.Args[3], 64)
+	//	if err != nil {
+	//		fmt.Println("ungültiger Wert für c")
+	//		return
+	//	}
+	p := params[1] / params[0]
+	q := params[2] / params[0]
+	fmt.Printf("x^2 + %.3f x + %.3f = 0\n", p, q)
 	d := -p / 2
 	Deskriminante := d*d + q
 	if Deskriminante < 0 {
