31ea15c30e5444c36176c81c3212960171b54028
Blue <cosinori@gmail.com>
2017-12-18 21:41:40 +0100

    add posibility to read contacts from json-file

--- adressbuch/adress.go
+++ adressbuch/adress.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
 type (
 	contact struct {
-		name    string
-		address string
-		phone   string
-		age     int
+		Name    string
+		Address string
+		Phone   string
+		Age     int
 	}
 	contact_M map[string]contact //neuer Typ (eine Map(Abbildung)), Index=string, typ=contact
 )
@@ -17,30 +19,40 @@ type (
 var (
 	kontakte_m = contact_M{
 		"Sarah": contact{
-			name:    "Sarah",
-			address: "99423 Weimar",
-			phone:   "017631254433",
-			age:     18},
+			Name:    "Sarah",
+			Address: "99423 Weimar",
+			Phone:   "017631254433",
+			Age:     18},
 		"Klaus": contact{
-			name:    "Klaus",
-			address: "Pragerstr. 42",
-			phone:   "015134876955",
-			age:     102},
+			Name:    "Klaus",
+			Address: "Pragerstr. 42",
+			Phone:   "015134876955",
+			Age:     102},
 		"Mia": contact{
-			name:    "Mia",
-			address: "Pragerstr. 42",
-			phone:   "015134876955",
-			age:     102},
+			Name:    "Mia",
+			Address: "Pragerstr. 42",
+			Phone:   "015134876955",
+			Age:     102},
 	}
 )
 
 func main() {
+	kontakte_b, err := ioutil.ReadFile("adressbuch.json")
+	if err != nil {
+		fmt.Println("Beim Lesen von adressbuch.json ist ein Fehler aufgetreten:", err)
+		return
+	}
+	err = json.Unmarshal(kontakte_b, &kontakte_m)
+	if err != nil {
+		fmt.Println("Beim Interpretieren von adressbuch.json ist ein Fehler aufgetreten:", err)
+		return
+	}
 	var kname string
 	fmt.Print("Gebt bitte einen Namen ein: ")
 	fmt.Scanln(&kname)
 	k, found := kontakte_m[kname]
 	if found {
-		fmt.Println(k.name, k.address, k.phone, k.age)
+		fmt.Println(k.Name, k.Address, k.Phone, k.Age)
 	} else {
 		fmt.Println("Haben wir nicht, überlegen sie sich was anderes. :D")
 	}
